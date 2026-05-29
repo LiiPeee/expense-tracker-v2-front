117bf60d30e7d4a1807205d224cb07627a45b539
liipeee <luiznuubb@gmail.com>
2026-05-29 20:15:10 -0300

    fix da rolagem de contato

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -58,7 +58,7 @@ const Contacts = () => {
                 Novo Contato
               </Button>
             </DialogTrigger>
-            <DialogContent className="border-white/60 bg-white/90 backdrop-blur-md sm:max-w-[560px] flex flex-col max-h-[90vh]">
+            <DialogContent className="border-white/60 bg-white/90 backdrop-blur-md sm:max-w-[560px] flex flex-col max-h-[90vh] overflow-hidden">
               <DialogHeader>
                 <DialogTitle>{editingContact ? "Editar Contato" : "Novo Contato"}</DialogTitle>
               </DialogHeader>
